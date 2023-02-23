0df14d841bd3c29499a3dfee9896d1417588a07f
sg-img7742 <sarveshgupta.img@gmail.com>
2023-02-23 18:45:19 +0530

    chnages in request api

--- app/Http/Controllers/Api/RequestApiController.php
+++ app/Http/Controllers/Api/RequestApiController.php
@@ -23,7 +23,7 @@ class RequestApiController extends Controller
                     $query->select(DB::raw('count(receiver_id)'));
                 }
             ])->orderBy('total_count', 'DESC')->get();
-            
+            // dd($get);
             // $userdetail = UserResource::collection($users);
             return ApiResponse::ok(
                 'Most Popular User Profile',
@@ -55,28 +55,31 @@ class RequestApiController extends Controller
             if ($validator->fails()) {
                 return $this->validation_error_response($validator);
             }
-            $reqdata['sender_id'] = $request['sender_id'];
-            $reqdata['receiver_id'] = $request['receiver_id'];
-
             $validated = $validator->validated();
 
-            $chk_id = User::where('id', $request->receiver_id)->orWhere('id', $request->sender_id)->where('phone_verified_at', '!=', null)->get();
+            $reqdata['sender_id'] = $validated['sender_id'];
+            $reqdata['receiver_id'] = $validated['receiver_id'];
+
+            $chk_id = User::where('id', $request->receiver_id)
+                ->where('id', $request->sender_id)
+                ->where('phone_verified_at', '!=', null)->get();
 
             if (!empty($chk_id)) {
                 $sender = Requests::where('sender_id', $id)->first();
                 $receiver = Requests::where('receiver_id', $id)->first();
-
-                // if (!empty($sender) && $request->sender_id == $id) { # leave it as a comment
-
-
-                if ($request->sender_id == $id) {
+                if ($request->sender_id  ==  $request->receiver_id) {
+                    return ApiResponse::error(
+                        'Invalid Request!!',
+                    );
+                } elseif ($request->sender_id == $id) {
                     if ($sender->status == 0) {
                         return ApiResponse::ok('Sorry!! Request has been already sent before!!');
                     } elseif ($sender->status == 1) {
                         return ApiResponse::ok('Your request already accepted!!');
                     } else {
                         $verified['status'] = 0;
-                        Requests::where('sender_id', $request->sender_id)->where('receiver_id', $request->receiver_id)->update($verified);
+                        Requests::where('sender_id', $request->sender_id)
+                            ->where('receiver_id', $request->receiver_id)->update($verified);
                         DB::commit();
                         return ApiResponse::ok('Your request has been sent again!!');
                     }
@@ -93,7 +96,6 @@ class RequestApiController extends Controller
                     $reqdata['sender_id'] = $request['sender_id'];
                     $reqdata['receiver_id'] = $request['receiver_id'];
                     $reqdata['status'] = $request['status'];
-
                     $validated = $validator->validated();
                     if ($request->status == 1) {
                         $verified['status'] = 1;
@@ -109,6 +111,7 @@ class RequestApiController extends Controller
                         return ApiResponse::error('Your request is pending');
                     }
                 } else {
+
                     Requests::create([
                         'sender_id' => $validated['sender_id'],
                         'receiver_id' => $validated['receiver_id'],
@@ -117,7 +120,6 @@ class RequestApiController extends Controller
                     $receiver_data = User::where('id', $request->receiver_id)->first();
                     return ApiResponse::ok(
                         'Request Has Been Sent Successfully!!',
-                        // $this->getRequestData($receiver_data)
                     );
                 }
             } else {
